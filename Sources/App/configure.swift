90214810c3b43bb5c751bac79582ac201e9d03a0
filipeilunga <filipeilunga@gmail.com>
2023-08-30 20:01:02 -0300

    websocket funcional

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -17,8 +17,8 @@ public func configure(_ app: Application) async throws {
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
-    app.migrations.add(CreateTodo())
-
+    app.migrations.add(CreateSongs())
+    try await app.autoMigrate().get()
     // register routes
     try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,14 +1,62 @@
 import Fluent
 import Vapor
 
+let room = Room()
+
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    app.webSocket("toki") { request, ws in
+        ws.onText { ws, text in
+            let messageSplited = text.components(separatedBy: "|")
+            
+            guard let userName = messageSplited.first else {
+                return
+            }
+            
+            let messageToSend = messageSplited[1] + "|" + messageSplited[2]
+            print(messageToSend)
+            room.connections[userName] = ws
+            room.send(userName: userName, newMessage: messageToSend)
+        }
     }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    
+    app.webSocket("isTappingSocket") { request, ws in
+        ws.onText { ws, text in
+            let messageSplited = text.components(separatedBy: "|")
+            
+            guard let userName = messageSplited.first, let message = messageSplited.last else {
+                return
+            }
+            
+            room.connections[userName] = ws
+            room.send(userName: userName, newMessage: message)
+        }
     }
-
-    try app.register(collection: TodoController())
+    
+    try app.register(collection: SongController())
 }
+
+struct User: Codable {
+    let userName: String
+    let message: String
+}
+
+struct Message: Codable {
+    let userName: String
+    let message: String
+    
+}
+
+class Room {
+    var connections =  [String: WebSocket]()
+    
+    func send(userName: String, newMessage: String)  {
+        let message = userName + "|" + newMessage
+        
+        for (user, websocket) in connections {
+            guard user != userName else { continue }
+            websocket.send(message)
+        }
+    }
+}
+
+
